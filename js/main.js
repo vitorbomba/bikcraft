3c89d07c09280caf91c5c63730c152bf225ddc52
vitorbomba <vitor.bomba@terra.com.br>
2017-10-27 22:28:32 -0200

    versao 27.10

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(".rslides").responsiveSlides({
+    auto: true,
+    speed: 500,
+    timeout: 5000,
+});
+
+$(".rslides_portfolio").responsiveSlides({
+    auto: true,
+    speed: 500,
+    timeout: 4000,
+});
